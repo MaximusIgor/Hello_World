26e157fb97c69c86def8ee839241b1a746fc7b48
Shylov Ihor <shilov0209@gmail.com>
2017-07-19 16:32:14 +0000

    Update HelloWorld.java

--- src/ShylovIhorSummer2017/HelloWorld.java
+++ src/ShylovIhorSummer2017/HelloWorld.java
@@ -2,8 +2,7 @@ package ShylovIhorSummer2017;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        for (int i = 0; i < 3; i++) {
+        
             System.out.println("Hello World");
         }
     }
-}
